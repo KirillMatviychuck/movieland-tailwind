ce5f4400092f83ef922fa9e72fa9cf6615def0aa
Kirill <k.matviychuck@gmail.com>
2024-07-10 10:28:38 +0300

    add scroll top method when pagination btn pressed

--- src/components/Pagination/PaginationBtn/PaginationBtn.tsx
+++ src/components/Pagination/PaginationBtn/PaginationBtn.tsx
@@ -5,7 +5,10 @@ import { getMovieList } from "../../../redux/slices/movie-list/movie-list-slice"
 const PaginationBtn: FC<PaginationBtnProps> = ({ sign, activePage, value }) => {
     const dispatch = useAppDispatch()
     const { currentCategory } = useAppSelector(state => state.movieListSlice)
-    const onClickHandler = () => dispatch(getMovieList({ page: value, movieCategory: currentCategory }))
+    const onClickHandler = () => {
+        dispatch(getMovieList({ page: value, movieCategory: currentCategory }))
+        window.scrollTo(0, 0);
+    }
 
     return (
         <button className={`box-border px-3 py-1 mx-1 ${activePage === value ? 'bg-slate-400' : 'bg-slate-300'} rounded-md`} onClick={onClickHandler} disabled={sign ? true : false}>
